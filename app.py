9b896b0e025c6c75da9f02f1efd0aab6b686761b
KotaYuno <84302413+KotaYuno@users.noreply.github.com>
2022-02-25 23:30:58 +0900

    Update app.py

--- app.py
+++ app.py
@@ -29,8 +29,8 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+# # socketio.run(app, debug=True)
 # socketio.run(app, debug=True)
-socketio.run(app, debug=True)
 
 #key:room名, 値:パスワード
 roomes = {}
